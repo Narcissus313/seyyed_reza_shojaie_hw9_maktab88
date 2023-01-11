bfe59fc4169f1d4b3357b7e1f53432614a1a17dc
narcissus313 <srz.shl@gmail.com>
2023-01-11 19:01:09 +0330

    up to end

--- 2_1/js/main.js
+++ 2_1/js/main.js
@@ -7,19 +7,18 @@ let modalBody = document.querySelector(".modal-body");
 let btnSearch = document.getElementById("btnSearch");
 let pagesUl = document.getElementById("pagesUl");
 let form = document.querySelector("#form");
-let params = Object.keys(users[0]);
-let numberOfCardsPerpage = 6;
-let lastUsers;
-// let pageNumber = Math.ceil(users.length / numberOfCardsPerpage);
+let params = Object.keys(users_data[0]);
+let numberOfCardsPerPage = 6;
+let lastUsers = users_data;
 let currentPage = 1;
-let inputSearchbarValue;
+let inputSearchBarValue;
 
 function formPreventFromRefresh(event) {
 	event.preventDefault();
 }
 
-function userProfileCreator(data) {
-	const targetUser = users.find((user) => user.id == data);
+function userProfileModalCreator(data) {
+	const targetUser = user - data.find((user) => user.id == data);
 	modalTitle.innerText = targetUser.first_name + " " + targetUser.last_name;
 	modalBody.innerHTML = `
     <div class="row p-3">
@@ -34,17 +33,17 @@ function userProfileCreator(data) {
 }
 
 function userCardMaker(user) {
-	user.localImagePlace = `./images/${user.id}-image.jpg`;
+	user.localImagePlace = `./images/${user["id"]}-image.jpg`;
 	return `<div class="col-md-4 col-sm-6 my-1">
                 <div class="card px-2 shadow h-100">
-                    <img src="${user.localImagePlace}" class="card-img-top my-2 rounded-3">
+                     <img src="${user.localImagePlace}" class="card-img-top my-2 rounded-3">
                     <h5 class="card-title my-3 text-center fw-bold">${user.first_name} ${user.last_name}</h5>
                     <ul class="list-group list-group-flush h-25 fs-6">
                         <li class="list-group-item p-2">id: ${user.id}</li>
                         <li class="list-group-item p-2">email: ${user.email}</li>
                     </ul>
                     <button
-                    onclick='userProfileCreator(${user.id})'
+                    onclick='userProfileModalCreator(${user.id})'
                     class="btn btn-primary my-3"
                     data-bs-toggle="modal" data-bs-target="#userProfileModal"
                     >
@@ -53,14 +52,13 @@ function userCardMaker(user) {
             </div>`;
 }
 function usersListGenerator(userList) {
-    console.log('userList: ', userList);
 	let cards = "";
 	for (
-		let i = numberOfCardsPerpage * (currentPage - 1);
-		i < numberOfCardsPerpage * currentPage;
+		let i = currentPage * numberOfCardsPerPage - numberOfCardsPerPage;
+		i < currentPage * numberOfCardsPerPage;
 		i++
 	) {
-		if (!userList[i].length) return cards;
+		if (!userList[i]) return cards;
 		cards += userCardMaker(userList[i]);
 	}
 	return cards;
@@ -68,37 +66,46 @@ function usersListGenerator(userList) {
 
 function renderUsersList(customUsers) {
 	usersListRow.innerHTML = "";
-    usersListRow.innerHTML += usersListGenerator(customUsers);
-    pageLinksCreator(customUsers);
+	usersListRow.innerHTML += usersListGenerator(customUsers);
+	pageLinksCreator(customUsers);
 }
 function searchInUsers(value) {
-	const filteredUsers = users.filter((item) => {
+	const filteredUsers = users_data.filter((item) => {
 		for (const param of params) {
 			if (param === "avatar") continue;
 			if (String(item[param]).toLowerCase().includes(value.toLowerCase()))
 				return true;
 		}
-    });
-    lastUsers = filteredUsers;
-	renderUsersList(filteredUsers);
+	});
+	currentPage = 1;
+	if (!filteredUsers.length) {
+		usersListRow.innerHTML = `<h1 style="color:red;">Unfortunately Nothing found!!</h1>`;
+		pagesUl.innerHTML = "";
+		return;
+	}
+	lastUsers = filteredUsers;
+	renderUsersList(lastUsers);
 }
 
 btnSearch.addEventListener("click", function () {
-	searchInUsers(inputSearchbarValue);
+	searchInUsers(inputSearchBarValue);
 });
 
 inputSearchbar.addEventListener("keyup", function (e) {
-	inputSearchbarValue = inputSearchbar.value;
-	if (inputSearchbarValue === "") renderUsersList(users);
-	if (e.key === "Enter") searchInUsers(inputSearchbarValue);
+	inputSearchBarValue = inputSearchbar.value;
+	if (inputSearchBarValue === "") {
+		lastUsers = users_data;
+		renderUsersList(lastUsers);
+	}
+	if (e.key === "Enter") searchInUsers(inputSearchBarValue);
 });
 
-function pageLinksCreator(users) {
-    let pageNumber = Math.ceil(users.length / numberOfCardsPerpage);
-    pagesUl.innerHTML = '';
-	for (let i = 1; i < pageNumber+1; i++) {
+function pageLinksCreator(users_data) {
+	let pageNumber = Math.ceil(users_data.length / numberOfCardsPerPage);
+	pagesUl.innerHTML = "";
+	for (let i = 1; i < pageNumber + 1; i++) {
 		pagesUl.innerHTML += `<li class="page-item ${
-			i == 1 ? "active" : ""
+			i == currentPage ? "active" : ""
 		}"><a class="page-link" href="#">${i}</a></li>`;
 	}
 }
@@ -108,9 +115,8 @@ pagesUl.addEventListener("click", function (e) {
 	}
 	e.target.parentElement.className += " active";
 	currentPage = +e.target.innerText;
-    // console.log('currentPage: ', currentPage);
 	renderUsersList(lastUsers);
 });
 
 form.addEventListener("submit", formPreventFromRefresh);
-renderUsersList(users);
+renderUsersList(lastUsers);
