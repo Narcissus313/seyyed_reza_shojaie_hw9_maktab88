ff503de61a6dc96fb457ae90a41a83edca4b0b01
narcissus313 <srz.shl@gmail.com>
2023-01-16 21:02:50 +0330

    minor change

--- 7/script.js
+++ 7/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-	let a = 0;
+	let progressPercent = 0;
 	$(".btn-add-ten").click(function () {
-		if (a >= 100) {
+		if (progressPercent >= 100) {
 			alert("no further step is allowed!");
 			return;
 		}
-		a += 10;
-		$("span").text(a.toFixed().toString() + "%");
-		$(".percent").css("width", "" + a + "%");
-		if (a >= 91) {
+		progressPercent += 10;
+		$("span").text(progressPercent.toFixed().toString() + "%");
+		$(".percent").css("width", "" + progressPercent + "%");
+		if (progressPercent >= 91) {
 			$(".percent").css("width", "" + 100 + "%");
 			setTimeout(() => {
 				alert("finished");
