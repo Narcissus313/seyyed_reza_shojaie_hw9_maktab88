98385fbac5adff3caca5001d38e08ebbbff80970
narcissus313 <srz.shl@gmail.com>
2023-01-18 14:00:17 +0330

    i'm tired end!!!!!

--- 8/script.js
+++ 8/script.js
@@ -1,12 +1,18 @@
 function myFunction(id) {
 	if (!$(`#${id}`).hasClass("w3-show")) {
+		$(".w3-container>div").removeClass("w3-show");
+		$(".w3-container>div").addClass("w3-hide");
+
 		$(`#${id}`).removeClass("w3-hide");
 		$(`#${id}`).addClass("w3-show");
-		$(`#button1>i`).removeClass("fa-caret-down");
-		$(`#button1>i`).addClass("fa-caret-right");
+
+		let btnCarret = `#button` + String(`#${id}`.match(/[0-9]/g)) + `>i`;
+		$(btnCarret).removeClass("fa-caret-down");
+		$(btnCarret).addClass("fa-caret-right");
 	} else {
-		$(`#${id}`).removeClass("w3-show");
-		$(`#${id}`).addClass("w3-hide");
+		$(".w3-container>div").removeClass("w3-show");
+		$(".w3-container>div").addClass("w3-hide");
+
 		$(`.w3-button>i`).removeClass("fa-caret-right");
 		$(`.w3-button>i`).addClass("fa-caret-down");
 	}
